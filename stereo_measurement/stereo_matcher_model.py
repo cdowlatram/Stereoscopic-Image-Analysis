fa1eeeac7b7b9c15c703870f024de8e4c92c6e4e
Steven Calderon <steve@scldrn.com>
2019-10-25 16:25:49 -0400

    Updated script to return invalid/valid points (whichever is less).

--- stereo_measurement/stereo_matcher_model.py
+++ stereo_measurement/stereo_matcher_model.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import numpy as np
 import argparse
 import json
@@ -6,17 +8,11 @@ import math
 
 
 class Stereograph:
-    def __init__(self, left_image_path, right_image_path):
-        self.imgL = cv.imread(left_image_path, 0)
-        self.imgR = cv.imread(right_image_path, 0)
-
-        # resize for faster computation
-        scale_percent = 800/self.imgL.shape[1] * 100  # percent of original size, target_size/current_size
-        width = int(self.imgL.shape[1] * scale_percent / 100)
-        height = int(self.imgL.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        self.imgL = cv.resize(self.imgL, dim, interpolation=cv.INTER_AREA)
-        self.imgR = cv.resize(self.imgR, dim, interpolation=cv.INTER_AREA)
+    def __init__(self, left_image, right_image, focal_length_mm, sensor_width_mm):
+        self.imgL = left_image
+        self.imgR = right_image
+        self.focal_length_mm = float(focal_length_mm)
+        self.sensor_width_mm = float(sensor_width_mm)
 
         # SBGM Settings
         self.window_size = 9
@@ -31,6 +27,26 @@ class Stereograph:
         self.speckleRange = 1   # Normally, 1 or 2 is good enough
         self.speckleWindowSize = 100  # max size of smooth disparity to consider noise and invalidate. 0 disable, 50-200
 
+        self.disparity = None
+        self.image_map = None
+        self.image_map_points = {}
+        self.scale = 1
+
+        self.points = np.empty(0)
+        self.colors = np.empty(0)
+
+        # Calculates Disparity
+        self.update_settings(self.min_disp, self.num_disp, self.window_size, self.uniquenessRatio, self.speckleWindowSize, self.speckleRange, self.disp12MaxDiff)
+
+    def update_settings(self, min_disp, num_disp, window_size, uniquenessRatio, speckleWindowSize, speckleRange, disp12MaxDiff):
+        self.min_disp = min_disp
+        self.num_disp = num_disp
+        self.window_size = window_size
+        self.blockSize = window_size
+        self.uniquenessRatio = uniquenessRatio
+        self.speckleWindowSize = speckleWindowSize
+        self.speckleRange = speckleRange
+        self.disp12MaxDiff = disp12MaxDiff
 
         self.stereo = cv.StereoSGBM_create(
             minDisparity=self.min_disp,
@@ -44,106 +60,182 @@ class Stereograph:
             speckleRange=self.speckleRange
         )
 
-        self.disparity = None
-        self.image_map_points = None
-        self.scale = 1
-
-        # Calculates Disparity
-        self.update_settings(self.window_size, self.uniquenessRatio, self.speckleWindowSize, self.speckleRange, self.disp12MaxDiff)
-
-    def update_settings(self, window_size, uniquenessRatio, speckleWindowSize, speckleRange, disp12MaxDiff):
-        self.window_size = window_size
-        self.blockSize = window_size
-        self.stereo.setBlockSize(window_size)
-
-        self.uniquenessRatio = uniquenessRatio
-        self.stereo.setUniquenessRatio(uniquenessRatio)
-
-        self.speckleWindowSize = speckleWindowSize
-        self.stereo.setSpeckleWindowSize(speckleWindowSize)
-
-        self.speckleRange = speckleRange
-        self.stereo.setSpeckleRange(speckleRange)
-
-        self.disp12MaxDiff = disp12MaxDiff
-        self.stereo.setDisp12MaxDiff(disp12MaxDiff)
-
         self.disparity = self.stereo.compute(self.imgL, self.imgR).astype(np.float32) / 16.0
 
-    def show_disparity(self):
-        cv.imshow('disparity result', (self.disparity - self.min_disp) / self.num_disp)
+    def show_disparity(self, title):
+        cv.imshow(title, (self.disparity - self.min_disp) / self.num_disp)
 
-    def generate_3d_points(self):
+    def set_3d_points(self):
         h, w = self.imgL.shape[:2]
+
         f = 0.8 * w  # guess for focal length
+
+        # If you know the sensor's physical width in mm, then the focal in pixels is:
+        # focal_pixel = (focal_mm / sensor_width_mm) * image_width_in_pixels
+        f = (self.focal_length_mm / self.sensor_width_mm) * w
+
+        # If you know the horizontal field of view, say in degrees,
+        # focal_pixel = (image_width_in_pixels * 0.5) / tan(FOV * 0.5 * PI/180)
+
         Q = np.float32([[1, 0, 0, -0.5 * w],
                         [0, -1, 0, 0.5 * h],  # turn points 180 deg around x-axis,
                         [0, 0, 0, -f],  # so that y-axis looks up
                         [0, 0, 1, 0]])
-        points = cv.reprojectImageTo3D(self.disparity, Q)
-        colors = cv.cvtColor(self.imgL, cv.COLOR_BGR2RGB)
-        mask = self.disparity > self.disparity.min()
-        out_points = points[mask]
-        out_colors = colors[mask]
+        self.points = cv.reprojectImageTo3D(self.disparity, Q)
+        self.colors = cv.cvtColor(self.imgL, cv.COLOR_BGR2RGB)
+
+    def get_pixels_allowed(self):
+        if self.points.size is 0:
+            self.set_3d_points()
+
+        h, w = self.imgL.shape[:2]
+
+        verts = self.points
+        colors = self.colors
 
         # Get image coordinates
         ixs = np.tile(np.arange(w), (h,1))
         iys = np.tile(np.arange(h), (w,1)).transpose()
-        out_ixs = ixs[mask]
-        out_iys = iys[mask]
 
-        # Initialize image map points array
-        self.image_map_points = [[dict() for y in range(h)] for x in range(w)]
-        self.generate_mapping(out_points, out_colors, out_ixs, out_iys)
+        # Initialize image map
+        self.image_map = np.zeros([h,w,3],dtype=np.uint8)
+        # self.image_map.fill(255)  # white image
 
-    def generate_mapping(self, verts, colors, ixs, iys):
+        # Determine if there a more dead pixels or live pixels
+        total_original_points = self.points.size
+
+        # Getting only valid points
+        valid_disp_mask = self.disparity > self.disparity.min()
+        lverts = self.points[valid_disp_mask]
+
+        if total_original_points/2 >= lverts.size:
+            # Send back live points
+
+            colors = colors[valid_disp_mask]
+            ixs = ixs[valid_disp_mask]
+            iys = iys[valid_disp_mask]
+
+            remove_inf_nan = np.isfinite(lverts)
+            ixs = ixs[remove_inf_nan]
+            iys = iys[remove_inf_nan]
+            verts = lverts[remove_inf_nan]
+
+            self.image_map_points["is_valid"] = True
+        else:
+            # Send back dead points
+
+            invalid_disp_mask = self.disparity <= self.disparity.min()
+            dverts = self.points[invalid_disp_mask]
+
+            ixs = ixs[invalid_disp_mask]
+            iys = iys[invalid_disp_mask]
+            verts = dverts
+
+            self.image_map_points["is_valid"] = False
+
         verts = verts.reshape(-1, 3)
-        colors = colors.reshape(-1, 3)
         ixs = ixs.reshape(-1, 1)
         iys = iys.reshape(-1, 1)
 
-        verts = np.hstack([verts, colors, ixs, iys])
+        verts = np.hstack([verts, ixs, iys])
 
         for i, point in enumerate(verts):
             x = point[0]
             y = point[1]
             z = point[2]
-            r = int(point[3])
-            g = int(point[4])
-            b = int(point[5])
-            Ix = int(point[6])
-            Iy = int(point[7])
+            Ix = int(point[3])
+            Iy = int(point[4])
 
-            self.image_map_points[Ix][Iy] = {
+            values = {
                 "x": x,
                 "y": y,
                 "z": z,
-                "r": r,
-                "g": g,
-                "b": b
             }
+
+            if Ix not in self.image_map_points:
+                self.image_map_points[Ix] = {Iy: values}
+            elif Iy not in self.image_map_points[Ix]:
+                self.image_map_points[Ix][Iy] = values
+            else:
+                del self.image_map_points[Ix][Iy]
+                self.image_map_points[Ix][Iy] = values
+
+        return self.image_map_points
+
+    def write_ply(self, filename):
+        ply_header ='''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+        if self.points.size is 0:
+            self.set_3d_points()
+
+        # Getting only valid disparity points
+        mask = self.disparity > self.disparity.min()
+        verts = self.points[mask]
+        colors = self.colors[mask]
+
+        # Remove NaN and Inf values
+        mask = np.isfinite(verts)
+        verts = verts[mask]
+        colors = colors[mask]
+
+        verts = verts.reshape(-1, 3)
+        colors = colors.reshape(-1, 3)
+        verts2 = np.hstack([verts, colors])
+
+        with open(filename, 'wb') as f:
+            f.write((ply_header % dict(vert_num=len(verts2))).encode('utf-8'))
+            np.savetxt(f, verts2, fmt='%f %f %f %d %d %d ')
 
     def calculate_distance(self, ix1, iy1, ix2, iy2):
         pt1 = self.image_map[ix1][iy1]
         pt2 = self.image_map[ix2][iy2]
         distance = math.sqrt((pt1.x-pt2.x)**2 + (pt1.y-pt2.y)**2 + (pt1.z-pt2.z)**2)
-        return distance
+        return distance * self.scale
 
-    def calculate_scale(self, ix1, iy1, ix2, iy2, actual_distance):
+    def calculate_set_scale(self, ix1, iy1, ix2, iy2, actual_distance):
         distance = self.calculate_distance(ix1, iy1, ix2, iy2)
         self.scale = actual_distance/distance
 
     def __str__(self):
         return 'Stereograph'
 
+def resize_image(image, target_size):
+    scale = target_size/image.shape[1]  # scale of original size, target_size/current_size
+    width = int(image.shape[1] * scale)
+    height = int(image.shape[0] * scale)
+    dim = (width, height)
+
+    return cv.resize(image, dim, interpolation=cv.INTER_AREA)
+
 
 if __name__ == "__main__":
+
+    def get_valid_points():
+        print(json.dumps(stereo.get_pixels_allowed()))
+
     ap = argparse.ArgumentParser(description="Calculate and return json stereograph 3d reconstruction map.")
+    ap.add_argument("mode", help="get_valid_points = Get valid/invalid points in json")
     ap.add_argument("image_left_path", help="path for image left")
     ap.add_argument("image_right_path", help="path for image right")
+    ap.add_argument("focal_length_mm", help="focal length of stereograph in mm")
+    ap.add_argument("sensor_width_mm", help="sensor width of stereograph in mm")
     args = vars(ap.parse_args())
 
-    stereo = Stereograph(args["image_left_path"], args["image_right_path"])
+    imgL = cv.imread(args["image_left_path"])
+    imgR = cv.imread(args["image_right_path"])
+    imgL = resize_image(imgL, 640)
+    imgR = resize_image(imgR, 640)
 
-    stereo.generate_3d_points()
-    print(json.dumps(stereo.image_map_points))
+    stereo = Stereograph(imgL, imgR, args["focal_length_mm"], args["sensor_width_mm"])
+
+    if args["mode"] == "get_valid_points":
+        get_valid_points()
