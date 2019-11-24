f54176c8c2bc3d87241fde69d960dc417db90401
Steven Calderon <steve@scldrn.com>
2019-11-24 10:51:58 -0500

    Created disparity map creation python script

---
+++ app/api/sources/python/disparity_maps.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+
+import stereo_matcher_model as smm
+import json
+import sys
+import os
+import cv2 as cv
+import numpy as np
+
+directory = os.path.dirname(os.path.realpath(__file__)) 
+directory = directory + "/../images/temp/"
+
+try:
+    imgL = cv.imread(sys.argv[1])
+    imgR = cv.imread(sys.argv[2])
+    imgL = smm.resize_image(imgL, 640)
+    imgR = smm.resize_image(imgR, 640)
+    stereo = smm.Stereograph(imgL, imgR, sys.argv[3], sys.argv[4])
+    stereo.update_settings(int(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]))
+    session = str(sys.argv[8])
+    
+    response = []
+    for i in range(1,3):
+        for j in range(2,7):
+            if i != j:
+                stereo.update_settings(i*16, j*16)
+                filename = session + "_" + str(i*16) + "_" + str(j*16) + ".jpg"
+                disparity_map = (stereo.get_disparity() * 255).round().astype(np.uint8)
+                cv.imwrite(directory + filename, disparity_map)
+                response.append({'min_disparity': i*16, 'num_disparity': j*16, 'image_name': filename})
+
+    print(json.dumps(response))
+    sys.stdout.flush()
+except Exception as e:
+    sys.stderr.write("Error encountered\n")
+    sys.stderr.write(repr(e))
+    sys.stderr.flush()
